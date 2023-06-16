e3166ee08ee689d7618fb8b03afe0269feb9c219
NikhilKulkarni5506 <136853172+NikhilKulkarni5506@users.noreply.github.com>
2023-06-16 15:00:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
